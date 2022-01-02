0fb07ddcd644a070b2d872cdb21f0fea092a728a
Gaston Caminiti <gastoncaminiti@outlook.com>
2022-01-02 15:44:04 -0300

    SceneManager and NextLevel
    Level 1  Scene
    Level 2 Scene
    StartCrashSequence Method
    StartNextSequence Method

--- EJEMPLO - RIGIDBODY Y FUERZA/Assets/Scripts/CollisionHandler.cs
+++ EJEMPLO - RIGIDBODY Y FUERZA/Assets/Scripts/CollisionHandler.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] private float delayTime = 1f;
+
     private void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag)
@@ -9,15 +12,51 @@ public class CollisionHandler : MonoBehaviour
             case "Friendly":
                 Debug.Log("IS A FRIEND");
                 break;
-            case "Fuel":
-                Debug.Log("IS A FUEL");
-                break;
             case "Finish":
-                Debug.Log("WIN");
+                StartNextSequence();
                 break;
             default:
-                Debug.Log("GAME OVER");
+                StartCrashSequence();
                 break;
         }
+    }
+
+    private void StartCrashSequence()
+    {
+        DisablePlayerControl();
+        Invoke("ReloadLevel", delayTime);
+    }
+
+    private void StartNextSequence()
+    {
+        DisablePlayerControl();
+        Invoke("LoadNextLevel", delayTime);
+    }
+
+    private void DisablePlayerControl()
+    {
+        GetComponent<Movement>().enabled = false;
+    }
+
+    private void ReloadLevel()
+    {
+        //RELOAD CODE
+        int curretSceneIndex = GetCurretSceneIndex();
+        SceneManager.LoadScene(curretSceneIndex);
+    }
+
+    private void LoadNextLevel()
+    {
+        int nextSceneIndex = GetCurretSceneIndex() + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private int GetCurretSceneIndex()
+    {
+        return SceneManager.GetActiveScene().buildIndex;
     }
 }
